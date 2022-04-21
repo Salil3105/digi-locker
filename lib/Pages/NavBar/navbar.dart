4b989749cc341ec7c582f499571f0586d765a48b
whitefeather <whitefeather@pop-os.localdomain>
2022-04-21 11:33:12 +0530

    Added NavBar Drawer Screens

--- lib/Pages/NavBar/navbar.dart
+++ lib/Pages/NavBar/navbar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
 
+// imports Screen
+import '../Home Page/home.dart';
+
 class NavBar extends StatefulWidget {
   const NavBar({Key? key}) : super(key: key);
 
@@ -9,17 +12,31 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  List<ScreenHiddenDrawer> _pages = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _pages = [
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Home",
+          baseStyle: TextStyle(),
+          selectedStyle: TextStyle(),
+        ),
+        Home(),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: HiddenDrawerMenu(
-        backgroundColorMenu: Colors.white10,
-        screens: [
-          
-        ],
-      ),
+    return HiddenDrawerMenu(
+      backgroundColorMenu: Colors.white10,
+      screens: _pages,
+      initPositionSelected: 0,
     );
   }
 }
-
-
